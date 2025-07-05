aeb146c4c90cf707a851159947dd04c66d6aa0c8
Abhishek Kumar <141398481+Abhi75033@users.noreply.github.com>
2025-07-05 17:21:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ import cookieParser from 'cookie-parser'
 
 const app = express()
 
-app.use(cors({origin: http://localhost:5173,
-    credentials: true})
+app.use(cors({ origin: 'http://localhost:5173',
+    credentials: true
+ }));
 
 
 app.use(express.json({limit:'16kb'}))
